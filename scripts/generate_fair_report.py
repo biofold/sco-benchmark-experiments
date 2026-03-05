7d7b4a656bb541d40857d5f88f734fab45eb04b6
emidio <emidio.capriotti@gmail.com>
2026-03-05 07:08:56 +0000

    update fair report

--- scripts/generate_fair_report.py
+++ scripts/generate_fair_report.py
@@ -1600,10 +1600,8 @@ class FAIRVisualizer:
         html_parts.append(f"""
         <header>
             <div class="header-content">
-                <div class="footer-content">
-                    Enhancing the FAIRness of the ELIXIR SCO Benchmark Experiments for Cancer Heterogeneity Analysis
-                </div>
-                
+                <h1><strong>FAIR Analysis Report</strong></h1>
+                <h4><strong>Transforming ELIXIR SCO benchmark experiments into FAIR-compliant, machine learning-ready resource/strong></h4>
                 <div>
                     <a href="../index.html" class="dashboard-link">
                         <i class="fas fa-home"></i> SCO Benchmark FAIR
@@ -1724,9 +1722,11 @@ class FAIRVisualizer:
         html_parts.append(f"""
         <footer id="footer">
             <div class="container">
-                <div class="footer-content">
-                    Enhancing the FAIRness of the ELIXIR SCO Benchmark Experiments for Cancer Heterogeneity Analysis
-                </div>
+                <h1>FAIR Analysis Report</h1>
+                <h4 style="color:white;"><strong>
+                    Transforming ELIXIR SCO benchmark experiments into FAIR-compliant, machine learning-ready resource
+                </strong></h4>
+                
                 <div class="footer-content" style="margin-top:25px;">
                     <a href="https://github.com/biofold/sco-benchmark-fair" target="_blank">
                     <img src="https://img.shields.io/badge/FAIR_Score-80.0%2F100-brightgreen" alt="FAIR Score: 80.0/100"></a>
